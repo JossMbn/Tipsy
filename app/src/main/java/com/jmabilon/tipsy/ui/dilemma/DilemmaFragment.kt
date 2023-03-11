29449c1dd0986f40348e0e7f7c1deb4734c38def
Josselin Mabilon <josselin.mabilon@epitech.eu>
2023-03-11 21:14:23 +0100

    create and fill style file, create dilemma fragment game, add new font

---
+++ app/src/main/java/com/jmabilon/tipsy/ui/dilemma/DilemmaFragment.kt
@@ -0,0 +1,14 @@
+package com.jmabilon.tipsy.ui.dilemma
+
+import android.os.Bundle
+import android.view.View
+import com.jmabilon.tipsy.databinding.FragmentDilemmaBinding
+import com.jmabilon.tipsy.extensions.abstract.AbsFragment
+
+class DilemmaFragment : AbsFragment<FragmentDilemmaBinding>(FragmentDilemmaBinding::inflate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
--- app/src/main/java/com/jmabilon/tipsy/ui/dilemma/DilemmaOnBoardingFragment.kt
+++ app/src/main/java/com/jmabilon/tipsy/ui/dilemma/DilemmaOnBoardingFragment.kt
@@ -1,9 +1,12 @@
 package com.jmabilon.tipsy.ui.dilemma
 
+import android.os.Build
 import android.os.Bundle
+import android.view.HapticFeedbackConstants
 import android.view.View
 import com.jmabilon.tipsy.databinding.FragmentDilemmaOnBoardingBinding
 import com.jmabilon.tipsy.extensions.abstract.AbsFragment
+import com.jmabilon.tipsy.extensions.android.safeNavigation
 
 class DilemmaOnBoardingFragment :
     AbsFragment<FragmentDilemmaOnBoardingBinding>(FragmentDilemmaOnBoardingBinding::inflate) {
@@ -11,5 +14,12 @@ class DilemmaOnBoardingFragment :
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.button.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                view.performHapticFeedback(HapticFeedbackConstants.CONFIRM)
+            }
+            val directions = DilemmaOnBoardingFragmentDirections.actionDilemmaOnBoardingFragmentToDilemmaFragment()
+            safeNavigation(directions)
+        }
     }
 }
