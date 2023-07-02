91c898f482be8a9ad7268b9da7162607f5f34bfa
Josselin Mabilon <josselin.mabilon@epitech.eu>
2023-07-02 16:06:08 +0200

    feat: add interstitial ads at the end of drink game

--- app/src/main/java/com/jmabilon/tipsy/ui/drinkgame/DrinkGameFragment.kt
+++ app/src/main/java/com/jmabilon/tipsy/ui/drinkgame/DrinkGameFragment.kt
@@ -8,6 +8,10 @@ import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.FullScreenContentCallback
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 import com.jmabilon.tipsy.R
 import com.jmabilon.tipsy.databinding.FragmentDrinkGameBinding
 import com.jmabilon.tipsy.extensions.android.safeNavigation
@@ -21,15 +25,22 @@ class DrinkGameFragment : AbsViewBindingFragment<FragmentDrinkGameBinding>() {
 
     private val viewModel: DrinkGameViewModel by viewModels()
 
+    private var interstitialAd: InterstitialAd? = null
+    private var adRequest: AdRequest? = null
+
     override fun getViewBinding(): FragmentDrinkGameBinding {
         return FragmentDrinkGameBinding.inflate(layoutInflater)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        adRequest = AdRequest.Builder().build()
         listOf(binding.adViewTop, binding.adViewBottom).forEach {
-            it.loadAd(AdRequest.Builder().build())
+            adRequest?.let { ad ->
+                it.loadAd(ad)
+            }
         }
+        setupInterstitialAds()
 
         binding.backIcon.setOnClickListener {
             performHapticFeedback()
@@ -44,6 +55,14 @@ class DrinkGameFragment : AbsViewBindingFragment<FragmentDrinkGameBinding>() {
         viewModel.loadData(resources.getStringArray(R.array.drink_game_player_questions_list))
     }
 
+    override fun onResume() {
+        super.onResume()
+        if (viewModel.uiState.value.gameIsFinish) {
+            val directions = DrinkGameFragmentDirections.actionDrinkGameFragmentToEndGameDialogFragment()
+            safeNavigation(directions)
+        }
+    }
+
     override fun initViewModelObservation() {
         super.initViewModelObservation()
 
@@ -55,9 +74,37 @@ class DrinkGameFragment : AbsViewBindingFragment<FragmentDrinkGameBinding>() {
                     binding.gameSentence.text = it
                 }
                 if (gameIsFinish) {
-                    val directions = DrinkGameFragmentDirections.actionDrinkGameFragmentToEndGameDialogFragment()
-                    safeNavigation(directions)
+                    if (interstitialAd != null) {
+                        interstitialAd?.show(requireActivity())
+                    } else {
+                        val directions = DrinkGameFragmentDirections.actionDrinkGameFragmentToEndGameDialogFragment()
+                        safeNavigation(directions)
+                    }
                 }
             }.launchIn(viewLifecycleOwner.lifecycleScope)
+    }
+
+    private fun setupInterstitialAds() {
+        adRequest?.let {
+            InterstitialAd.load(
+                requireContext(),
+                "ca-app-pub-2132066617984288/9567106784",
+                it,
+                object : InterstitialAdLoadCallback() {
+                    override fun onAdFailedToLoad(adError: LoadAdError) {
+                        interstitialAd = null
+                    }
+
+                    override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                        this@DrinkGameFragment.interstitialAd = interstitialAd
+                    }
+                })
+        }
+
+        interstitialAd?.fullScreenContentCallback = object: FullScreenContentCallback() {
+            override fun onAdDismissedFullScreenContent() {
+                interstitialAd = null
+            }
+        }
     }
 }
